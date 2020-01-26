9b912fbeee83170fa188a385bf0c8df36abae225
Neverender <tldavis09@yahoo.com>
2020-01-25 22:48:10 -0600

    o.o

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,14 @@
 // Working clock
 var $currentDay = $("#currentDay");
+var $hour9 = $("#hour-9");
+var $hour10 = $("#hour-10");
+var $hour11 = $("#hour-11");
+var $hour12 = $("#hour-12");
+var $hour13 = $("#hour-13");
+var $hour14 = $("#hour-14");
+var $hour15 = $("#hour-15");
+var $hour16 = $("#hour-16");
+var $hour17 = $("#hour-17");
 
 var currentHour = moment().hours();
 
@@ -12,3 +21,24 @@ function clock() {
 clock();
 
 console.log(currentHour);
+
+function turn9() {
+	$hour9.removeClass("past");
+	$hour9.addClass("present");
+}
+
+function tellTime() {
+	switch (currentHour) {
+		case 22:
+			turn9();
+			console.log("It's 10 o'clock");
+			break;
+		case 23:
+			console.log("It's 11 o'clock");
+			break;
+		case 24:
+			console.log("It's 12 o'clock");
+	}
+}
+
+tellTime();

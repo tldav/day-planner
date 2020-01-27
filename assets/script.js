2f850170424bd0f78fb2b8f32990339af97cfe1b
Neverender <tldavis09@yahoo.com>
2020-01-26 22:14:23 -0600

    added a function that keeps all timeblocks gray between 6 PM and midnight

--- assets/script.js
+++ assets/script.js
@@ -11,7 +11,7 @@ var $hour15 = $("#hour-15");
 var $hour16 = $("#hour-16");
 var $hour17 = $("#hour-17");
 
-// Array of hours
+// Array of hours. The placeholder items sync the array index with the current hour.
 var hourArray = [
 	0,
 	1,
@@ -35,6 +35,7 @@ var hourArray = [
 
 // Global Variables
 var getHour = 0;
+// Targets current hour with moment.js
 var currentHour = moment().hours();
 
 // Realtime date and time display
@@ -46,32 +47,48 @@ function clock() {
 
 clock();
 
-// Defaults all timeblocks to green starting at midnight
+// Keeps all timeblocks green between midnight and 9 AM
 function newDay() {
 	$("textarea").addClass("future");
 	$("textarea").removeClass("present");
 	$("textarea").removeClass("past");
 }
 
-//
+// Keeps all timeblocks gray between 6 PM and midnight
+function afterHours() {
+	hourArray[getHour].removeClass("future");
+	hourArray[getHour].removeClass("present");
+	hourArray[getHour].addClass("past");
+}
+
+// Handles the color change of each row
 function colorChange() {
 	newDay();
+
+	// Changes the row for each hour to red when the current time is within that hour
 	hourArray[getHour].removeClass("future");
 	hourArray[getHour].removeClass("past");
 	hourArray[getHour].addClass("present");
-	for (i = getHour - 1; i > 0; i--) {
+
+	// Loops through all past hours and turns their rows gray
+	for (var i = getHour - 1; i > 0; i--) {
 		hourArray[i].removeClass("future");
 		hourArray[i].removeClass("present");
 		hourArray[i].addClass("past");
 
+		// Ensures that the backwards loop doesn't try to target the placeholder array items, which will cause an error since classes cannot be added to them.
 		if (i <= 9) {
 			return;
 		}
 	}
 }
 
+// Tracks the hour of the day and targets the appropriate timeblock for color change
 function timeChange() {
 	switch (currentHour) {
+		default:
+			newDay();
+			break;
 		case 9:
 			getHour = hourArray.indexOf($hour9);
 			colorChange();
@@ -107,8 +124,14 @@ function timeChange() {
 		case 17:
 			getHour = hourArray.indexOf($hour17);
 			colorChange();
-		default:
-			newDay();
+			break;
+		case 18:
+		case 19:
+		case 20:
+		case 21:
+		case 22:
+		case 23:
+			afterHours();
 	}
 }
 
